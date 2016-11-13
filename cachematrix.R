1ba48facca83ba579d832a1632734d934922b2cc
ojasviwadhwa <ojasviwadhwa@outlook.com>
2016-11-13 20:05:32 +0530

    Commit after writing the 2 functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first Function creates a dummy matrix whose output can be provided to the next function ie. cacheSolve()
 
-## Write a short comment describing this function
+## makeCacheMatrix provides an instance of a matrix whose inverse can be calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  outputMatrix <- matrix(data = NA, nrow = nrow(x), ncol = ncol(x))
+  outputMatrix
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  outputMatrix <- matrix(data = NA, nrow = nrow(x), ncol = ncol(x))
+  endRow <- seq(1, nrow(x))
+  endCol <- seq(1, ncol(x))
+  for (i in endRow) {
+    for (j in endCol){
+      outputMatrix[i, j] <- x[j, i]
+    }
+  }
+  outputMatrix
 }
+
